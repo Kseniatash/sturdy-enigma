be458edd8db2e8e897591bd69feac01e4875e603
Ксения Ташкинова <taskinovak@gmail.com>
2023-09-25 18:17:31 +0500

    ДЗ задача 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -11,10 +11,17 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-if(number1 < number2)
-if (number2 > number1)
-{
-    Console.WriteLine($"{number1} является меньшим числом");
-    Console.WriteLine($"{number2} является большим числом");
-}
 
+if (number1 > number2)
+{
+           Console.WriteLine($"{number1} больше {number2}");
+}
+else 
+if (number1 < number2)
+{
+           Console.WriteLine($"{number1} меньше {number2}");
+}
+else
+{
+           Console.WriteLine($"{number1} равен {number2}");
+}
